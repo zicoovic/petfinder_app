6c1e33ad16d3419e7a46f286fed588d95007575a
zicoovic <abdelrhmanzakariaqa@gmail.com>
2025-10-14 08:15:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# petfinder_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/petfinder_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.petfinder_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
